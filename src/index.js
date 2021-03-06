c18ca5e45581470320048a3ee50ed3a654ebdf48
prokhorovd <github@prokhorov.dev>
2021-03-06 12:17:52 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  //let's go
+  let numberAsString = n.toString();
+  let reversedInt = "";
+  for (let i = 0; i < numberAsString.length; i++) {
+    if (numberAsString[i] === "-") {
+      i += 1;  
+    }
+    reversedInt = numberAsString[i] + reversedInt;
+  }
+  return reversedInt;
 }
